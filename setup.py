0479460557f5c7037d3311e2bf3746c12539f360
Karsten <karsten@einfach-schwarz.com>
2018-01-06 04:39:15 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = "headlines",
+        version = "0.1",
+        description = "Learn flask",
+        packages = find_packages(),
+        install_requires = ["gunicorn","flask"],
+        )

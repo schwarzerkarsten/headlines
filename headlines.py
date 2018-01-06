d22ccd7d0cff5ca34a3b3c8f980494a6a4215d80
Karsten <karsten@einfach-schwarz.com>
2018-01-06 21:07:16 +0100

    using get/input

--- headlines.py
+++ headlines.py
@@ -1,6 +1,7 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -12,10 +13,13 @@ RSS_FEEDS = {
 }
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="spiegel"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication="spiegel"
+    else:
+        publication=query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
     return render_template("home.html",articles=feed['entries'])
 
 if __name__ == '__main__':
